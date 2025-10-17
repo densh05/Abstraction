a93316ebb2c8e29268ee0413841c0e6271c42755
Denys Shaitan <schaitan.den@gmail.com>
2025-10-17 18:00:30 +0200

    Code changed

--- Abstraction/Program.cs
+++ Abstraction/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Threading.Channels;
 
 namespace Abstraction
 {
+    class ConcretClassA
+    {
+        public void Operation()
+        {
+            Console.WriteLine("Concrete class A");
+        }
+    }
+
+    abstract class AbstractClass : ConcretClassA
+    {
+        public abstract void Method();
+    }
+
+    class ConcretClassB : AbstractClass
+    {
+        public override void Method()
+        {
+            Console.WriteLine("Concrete class B");
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AbstractClass instance = new ConcretClassB();
+
+            instance.Operation();
+            instance.Method();
+
+
+            
         }
     }
 }
